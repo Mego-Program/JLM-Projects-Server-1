b06c322cdcf26d94167f3209f27c0570b3daadff
Yovegas <yehonathanbauer@gmail.com>
2023-12-22 01:38:42 +0200

    up

--- router/routerProject.js
+++ router/routerProject.js
@@ -19,7 +19,7 @@ const routerProject = express.Router();
 routerProject.post("/add_project",AddProject);
 routerProject.post("/add_member_to_project",AddMemberToProject);
 routerProject.post("/remove_member_from_project",RemoveMemberFromProject);
-routerProject.get("/",GetAllData);
+routerProject.post("/get_all_data",GetAllData);
 routerProject.post("/get_project_by_id",GetProjectByID);
 routerProject.post("/update_project_column_urgency",UpdateProjectColumnUrgency);
 routerProject.post("/update_project_column_text",UpdateProjectColumnText);
