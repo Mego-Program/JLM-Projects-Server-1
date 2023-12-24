5aa7ecab63a1c14e63751a3ca0ef136a6ed59f8a
Yovegas <yehonathanbauer@gmail.com>
2023-12-24 16:04:37 +0200

    Update sprint functionality

--- router/routerProject.js
+++ router/routerProject.js
@@ -17,12 +17,13 @@ import {
 const routerProject = express.Router();
 
 routerProject.post("/add_project",AddProject);
+routerProject.post("/delete_project",DeleteProject)
 routerProject.post("/add_member_to_project",AddMemberToProject);
 routerProject.post("/remove_member_from_project",RemoveMemberFromProject);
-routerProject.post("/get_all_data",GetAllData);
-routerProject.post("/get_project_by_id",GetProjectByID);
-routerProject.post("/update_project_column_urgency",UpdateProjectColumnUrgency);
-routerProject.post("/update_project_column_text",UpdateProjectColumnText);
+routerProject.post("/",GetAllBoards);
+routerProject.post("/get_project_by_id",getBoardById);
+routerProject.post("/update_project_column_urgency",updateColumnColor);
+routerProject.post("/update_project_column_text",updateColumnName);
 routerProject.post("/add_new_column",AddNewColumn);
 routerProject.post("/delete_column",DeleteColumn);
 routerProject.post("/add_sprint",AddSprints);
